76924a23a96efa5b7be089c640485c07b9b1dc87
laerkehansen <laha0007@stud.kea.dk>
2024-12-07 14:39:54 +0100

    singleview done

--- src/app/components/AristSingel.jsx
+++ src/app/components/AristSingel.jsx
@@ -4,35 +4,38 @@ const ArtistSingel = ({ band }) => {
   const { name, logo, members, genre, logoCredits, bio } = band;
   return (
     <div>
-      <div className="grid grid-cols-1 grid-rows-1 h-64">
-        <div className="w-full relative h-64 col-span-1 row-span-1">
-          <Image
-            src={getLogoUrl(logo)}
-            alt={`Billede af ${name}`}
-            fill={true}
-            className="object-cover "
-          />
-        </div>
-        <h1 className="text-green-500 text-6xl col-span-1 row-span-1 place-self-start z-10">
+      <div className="relative w-full mx-auto h-[500px] grid grid-cols-1 grid-rows-[1fr_0.2fr]">
+        <Image
+          src={getLogoUrl(logo)}
+          alt={`Billede af ${name}`}
+          fill={true}
+          className="object-cover grayscale"
+        />
+        <h1 className="text-green font-Inter text-8xl font-extrabold italic uppercase z-10 row-start-2 pl-4">
           {name}
         </h1>
       </div>
-      <div className="flex">
-        <p>{logoCredits}</p>
-      </div>
-
-      <div className="flex flex-row gap-10">
-        <div>
-          <h2 className="uppercase text-4xl">Medlemer</h2>
+      <section className="grid grid-cols-[0.1fr_0.5fr_1fr_0.1fr] py-16">
+        <div className="col-start-2">
+          <h2 className="uppercase text-6xl font-Inter italic font-extrabold pb-4">
+            Medlemer
+          </h2>
           {members.map((member, index) => (
-            <p key={index}>{member}</p>
+            <p key={index} className="font-Inter text-2xl">
+              {member}
+            </p>
           ))}
         </div>
         <div>
-          <h3 className="uppercase text-4xl">Om</h3>
-          <p>{bio}</p>
+          <h3 className="uppercase text-6xl font-Inter italic font-extrabold pb-4">
+            Om
+          </h3>
+          <p className="font-Inter text-2xl">{bio}</p>
+          <p className="font-Inter text-gray-400 pt-4">
+            Fotocredits: {logoCredits}
+          </p>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
