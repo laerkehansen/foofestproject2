ca3738346b3c41dc89d1b344d77a1b1e5e87c34c
kati0234 <katinkaasp@gmail.com>
2024-12-07 10:40:57 +0100

    singel sight

--- src/app/components/AristSingel.jsx
+++ src/app/components/AristSingel.jsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
+import { getLogoUrl } from "../lib/utils";
 const ArtistSingel = ({ band }) => {
+  const { name, logo, members, genre, logoCredits, bio } = band;
   return (
     <div>
-      <h1>{band.name}</h1>
+      <div className="grid grid-cols-1 grid-rows-1 h-64">
+        <div className="w-full relative h-64 col-span-1 row-span-1">
+          <Image
+            src={getLogoUrl(logo)}
+            alt={`Billede af ${name}`}
+            fill={true}
+            className="object-cover "
+          />
+        </div>
+        <h1 className="text-green-500 text-6xl col-span-1 row-span-1 place-self-start z-10">
+          {name}
+        </h1>
+      </div>
+      <div className="flex">
+        <p>{logoCredits}</p>
+      </div>
+
+      <div className="flex flex-row gap-10">
+        <div>
+          <h2 className="uppercase text-4xl">Medlemer</h2>
+          {/* <p>{members}</p> */}
+          {members.map((member, index) => (
+            <p key={index}>{member}</p>
+          ))}
+        </div>
+        <div>
+          <h3 className="uppercase text-4xl">Om</h3>
+          <p>{bio}</p>
+        </div>
+      </div>
     </div>
   );
 };
