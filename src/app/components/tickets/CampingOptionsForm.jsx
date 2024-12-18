6fd3bc3e525e0e4062b00c7133323597115700ef
laerkehansen <laha0007@stud.kea.dk>
2024-12-18 19:36:14 +0100

    campingsiden

--- src/app/components/tickets/CampingOptionsForm.jsx
+++ src/app/components/tickets/CampingOptionsForm.jsx
@@ -60,209 +60,205 @@ const CampingOptionsForm = ({ onNext, onBack, formData }) => {
       ...data,
     });
   };
-}
+};
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setValue, // bruges til at sætte værdier dynamisk, f.eks. når vi ændrer område på vores knapper
-    watch,
-  } = useForm({
-    resolver: zodResolver(validering),
-    defaultValues: {
-      campingSelected: formData.campingSelected || false, //bruges ik
-      addTentSetup: formData.addTentSetup || false, // Tilføj standardværdi for addTentSetup
-      vipCount: formData.vipCount || 0,
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+  setValue, // bruges til at sætte værdier dynamisk, f.eks. når vi ændrer område på vores knapper
+  watch,
+} = useForm({
+  resolver: zodResolver(validering),
+  defaultValues: {
+    campingSelected: formData.campingSelected || false, //bruges ik
+    addTentSetup: formData.addTentSetup || false, // Tilføj standardværdi for addTentSetup
+    vipCount: formData.vipCount || 0,
 
-      regularCount: formData.regularCount || 0,
-      area: formData.area || "",
-      tent2p: 0,
-      tent3p: 0,
-      // area: "",
-      greenCamping: false,
-    },
+    regularCount: formData.regularCount || 0,
+    area: formData.area || "",
+    tent2p: 0,
+    tent3p: 0,
+    // area: "",
+    greenCamping: false,
+  },
+});
+
+const [availableSpots, setAvailableSpots] = useState([]); // Tilgængelige områder
+const [selectedArea, setSelectedArea] = useState(null);
+const [loading, setLoading] = useState(true); //slet
+const [formError, setFormError] = useState("");
+
+const { updateCartData } = useContext(KviteringContext); // Få adgang til updateCartData
+
+useEffect(() => {
+  const subscription = watch((data) => {
+    const { area, tent2p, tent3p, greenCamping, addTentSetup } = data;
+    updateCartData({
+      area,
+      tent2p,
+      tent3p,
+      greenCamping,
+      addTentSetup,
+    });
   });
 
-  const [availableSpots, setAvailableSpots] = useState([]); // Tilgængelige områder
-  const [selectedArea, setSelectedArea] = useState(null);
-  const [loading, setLoading] = useState(true); //slet
-  const [formError, setFormError] = useState("");
+  // Cleanup, for at fjerne eventuelle subscriptioner
+  return () => subscription.unsubscribe();
+}, [watch, updateCartData]);
 
-  const { updateCartData } = useContext(KviteringContext); // Få adgang til updateCartData
+const fetchData = async () => {
+  const data = await getAvailableSpots();
+  setAvailableSpots(data); // Sætter de tilgængelige pladser
+  setLoading(false);
+};
 
-  useEffect(() => {
-    const subscription = watch((data) => {
-      const { area, tent2p, tent3p, greenCamping, addTentSetup } = data;
-      updateCartData({
-        area,
-        tent2p,
-        tent3p,
-        greenCamping,
-        addTentSetup,
-      });
+// Henter data, når komponenten er blevet rendere (kører kun én gang)
+useEffect(() => {
+  fetchData();
+  // const interval = setInterval(fetchData, 2000); // Tjek hver 2. sekund
+  // return () => clearInterval(interval);
+}, []);
+
+// Håndterer valget af et campingområde
+const handleAreaSelection = (area) => {
+  const selectedSpot = availableSpots.find((spot) => spot.area === area); // Finder det valgte område
+
+  const totalTickets = (formData.vipCount || 0) + (formData.regularCount || 0); // Beregn total billetter (VIP + Regular)
+
+  if (!selectedSpot || totalTickets > selectedSpot.available) {
+    setFormError(
+      `Området "${area}" er ikke tilgængeligt. Vælg et andet område.`
+    );
+    setSelectedArea(null);
+    setValue("area", ""); // Fjern værdien fra formularen
+  } else {
+    setFormError(""); // Fjern fejlmeddelelse
+    setSelectedArea(area);
+    setValue("area", area); // Sæt værdien i formularen
+
+    updateCartData({ area });
+    // Opdater de tilgængelige pladser
+    const updatedSpots = availableSpots.map((spot) => {
+      if (spot.area === area) {
+        spot.available -= totalTickets; // Træk de valgte billetter fra de tilgængelige pladser
+      }
+      return spot;
     });
+    setAvailableSpots(updatedSpots); // Opdaterer tilstanden med de nye tilgængelige pladser
+  }
+};
 
-    // Cleanup, for at fjerne eventuelle subscriptioner
-    return () => subscription.unsubscribe();
-  }, [watch, updateCartData]);
+// Håndterer plus og minus for telte
+const handleTentChange = (type, operation) => {
+  const currentValue = watch(type); // Hent det nuværende antal telte af den pågældende type
+  let newValue =
+    operation === "increment" ? currentValue + 1 : currentValue - 1;
+  if (newValue < 0) newValue = 0; // Undgå negative værdier
+  setValue(type, newValue);
 
-  const fetchData = async () => {
-    const data = await getAvailableSpots();
-    setAvailableSpots(data); // Sætter de tilgængelige pladser
-    setLoading(false);
-  };
+  updateCartData({ [type]: newValue });
+};
 
-  // Henter data, når komponenten er blevet rendere (kører kun én gang)
-  useEffect(() => {
-    fetchData();
-    // const interval = setInterval(fetchData, 2000); // Tjek hver 2. sekund
-    // return () => clearInterval(interval);
-  }, []);
+const onSubmit = async (data) => {
+  const selectedSpot = availableSpots.find((spot) => spot.area === data.area); // Find det valgte område
 
-  // Håndterer valget af et campingområde
-  const handleAreaSelection = (area) => {
-    const selectedSpot = availableSpots.find((spot) => spot.area === area); // Finder det valgte område
+  const totalTickets = (formData.vipCount || 0) + (formData.regularCount || 0); //hvor pladser er der af hver, er der ingen gør vi værdien er 0, så den ikke er undefined og giver os problemer
 
-    const totalTickets = // Beregn total billetter (VIP + Regular)
-      (formData.vipCount || 0) + (formData.regularCount || 0);
+  if (!data.area) {
+    setFormError("Du skal vælge et campingområde!"); // Sæt fejlmeddelelse
+    return;
+  }
 
-    if (!selectedSpot || totalTickets > selectedSpot.available) {
-      setFormError(
-        `Området "${area}" er ikke tilgængeligt. Vælg et andet område.`
-      );
-      setSelectedArea(null);
-      setValue("area", ""); // Fjern værdien fra formularen
-    } else {
-      setFormError(""); // Fjern fejlmeddelelse
-      setSelectedArea(area);
-      setValue("area", area); // Sæt værdien i formularen
+  if (totalTickets > selectedSpot.available) {
+    setFormError(
+      `Der er kun ${selectedSpot.available} billetter tilgængelige i ${selectedSpot.area}.`
+    );
+    return;
+  }
 
-      updateCartData({ area });
-      // Opdater de tilgængelige pladser
-      const updatedSpots = availableSpots.map((spot) => {
-        if (spot.area === area) {
-          spot.available -= totalTickets; // Træk de valgte billetter fra de tilgængelige pladser
-        }
-        return spot;
-      });
-      setAvailableSpots(updatedSpots); // Opdaterer tilstanden med de nye tilgængelige pladser
-    }
-  };
+  // API-opkald
+  try {
+    const result = await putReserveSpot(
+      data.area,
+      formData.vipCount || 0,
+      formData.regularCount || 0
+    );
+    alert("Reservationen er gennemført!");
+    onNext(data);
+  } catch (error) {
+    setFormError("Der opstod en fejl ved reservationen. Prøv igen senere.");
+  }
 
-  // Håndterer plus og minus for telte
-  const handleTentChange = (type, operation) => {
-    const currentValue = watch(type); // Hent det nuværende antal telte af den pågældende type
-    let newValue =
-      operation === "increment" ? currentValue + 1 : currentValue - 1;
-    if (newValue < 0) newValue = 0; // Undgå negative værdier
-    setValue(type, newValue);
+  // Hvis ingen fejl
+  setFormError(""); // Ryd fejl
+  onNext({
+    ...data,
+  });
+};
 
-    updateCartData({ [type]: newValue });
-  };
+return (
+  <>
+    <ReservationTimer
+      reservationId="1233" //HER GÅR DET GALT
+      onTimeout={(id) => console.log("Timeout triggered for:", id)}
+      onConfirm={(id) => console.log("Confirmed reservation for:", id)}
+    />
+    <div className="h-svh">
+      <div className="">
+        <p>Valgte billetter:</p>
+        <ul>
+          <li>VIP Billetter: {formData.vipCount}</li>
+          <li>Regular Billetter: {formData.regularCount}</li>
+        </ul>
+      </div>
 
-  const onSubmit = async (data) => {
-    const selectedSpot = availableSpots.find((spot) => spot.area === data.area); // Find det valgte område
+      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 ">
+        <div className="grid grid-rows-2 gap-4">
+          <h2 className="font-Inter text-lg pb-2">vælg camping område</h2>
 
-    const totalTickets =
-      (formData.vipCount || 0) + (formData.regularCount || 0); //hvor pladser er der af hver, er der ingen gør vi værdien er 0, så den ikke er undefined og giver os problemer
+          <div className="flex flex-row gap-4  ">
+            {availableSpots.map((spot, index) => {
+              const totalTickets =
+                (formData.vipCount || 0) + (formData.regularCount || 0); //udskriv totalen af billetter, de har begge en fallback værdi på 0, så vi ikke kan få undefinde
+              const isDisabled = totalTickets > spot.available; // Check for for mange billetter
 
-    if (!data.area) {
-      setFormError("Du skal vælge et campingområde!"); // Sæt fejlmeddelelse
-      return;
-    }
-
-    if (totalTickets > selectedSpot.available) {
-      setFormError(
-        `Der er kun ${selectedSpot.available} billetter tilgængelige i ${selectedSpot.area}.`
-      );
-      return;
-    }
-
-    // API-opkald
-    try {
-      const result = await putReserveSpot(
-        data.area,
-        formData.vipCount || 0,
-        formData.regularCount || 0
-      );
-      alert("Reservationen er gennemført!");
-      onNext(data);
-    } catch (error) {
-      setFormError("Der opstod en fejl ved reservationen. Prøv igen senere.");
-    }
-
-    // Hvis ingen fejl
-    setFormError(""); // Ryd fejl
-    onNext({
-      ...data,
-    });
-  };
-
-  return (
-    <>
-      <ReservationTimer
-        reservationId="1233" //HER GÅR DET GALT
-        onTimeout={(id) => console.log("Timeout triggered for:", id)}
-        onConfirm={(id) => console.log("Confirmed reservation for:", id)}
-      />
-      <div className="h-svh">
-        <div className="">
-          <p>Valgte billetter:</p>
-          <ul>
-            <li>VIP Billetter: {formData.vipCount}</li>
-            <li>Regular Billetter: {formData.regularCount}</li>
-          </ul>
-        </div>
-
-        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 ">
-          <div className="grid grid-rows-2 gap-4">
-            <h2 className="font-Inter text-lg pb-2">vælg camping område</h2>
-
-            <div className="flex flex-row gap-4  ">
-              {availableSpots.map((spot, index) => {
-                const totalTickets =
-                  (formData.vipCount || 0) + (formData.regularCount || 0); //udskriv totalen af billetter, de har begge en fallback værdi på 0, så vi ikke kan få undefinde
-                const isDisabled = totalTickets > spot.available; // Check for for mange billetter
-
-                return (
-                  <div key={index}>
-                    <input
-                      className="hidden peer"
-                      type="radio"
-                      id={spot.area}
-                      value={spot.area}
-                      {...register("area")} // Binding til formular
-                      onChange={() => handleAreaSelection(spot.area)} // Validering
-                      disabled={isDisabled} // Deaktiver området hvis der er for mange billetter
-                    />
-                    <label
-                      htmlFor={spot.area}
-                      className={`rounded-md cursor-pointer bg-slate-400 p-2 text-center font-Inter uppercase text-lg
+              return (
+                <div key={index}>
+                  <input
+                    className="hidden peer"
+                    type="radio"
+                    id={spot.area}
+                    value={spot.area}
+                    {...register("area")} // Binding til formular
+                    onChange={() => handleAreaSelection(spot.area)} // Validering
+                    disabled={isDisabled} // Deaktiver området hvis der er for mange billetter
+                  />
+                  <label
+                    htmlFor={spot.area}
+                    className={`rounded-md cursor-pointer bg-slate-400 p-2 text-center font-Inter uppercase text-lg
           peer-checked:bg-customPink-700 peer-checked:text-white
           hover:bg-gray-200 transition-all duration-200
           ${isDisabled ? "opacity-50 cursor-not-allowed" : ""}`}
-                    >
-                      {spot.area}
-                    </label>
-                    <div className="flex">
-                      <p>Tilgængelige pladser:</p>{" "}
-                      <p>
-                        {spot.available}/{spot.spots}
-                      </p>
-                    </div>
+                  >
+                    {spot.area}
+                  </label>
+                  <div className="flex">
+                    <p>Tilgængelige pladser:</p>{" "}
+                    <p>
+                      {spot.available}/{spot.spots}
+                    </p>
                   </div>
-                );
-              })}
-            </div>
-            {formError && (
-              <p className="text-red-500 text-sm mt-2">{formError}</p>
-            )}
+                </div>
+              );
+            })}
           </div>
           {formError && (
-            <p className="text-red-500 text-sm mb-4">{formError}</p>
+            <p className="text-red-500 text-sm mt-2">{formError}</p>
           )}
-          {selectedArea && <p>Du har valgt campingområde: {selectedArea}</p>}
+        </div>
+        {formError && <p className="text-red-500 text-sm mb-4">{formError}</p>}
+        {selectedArea && <p>Du har valgt campingområde: {selectedArea}</p>}
 
         {/* Grøn camping */}
         <div className="py-4">
@@ -381,17 +377,16 @@ const CampingOptionsForm = ({ onNext, onBack, formData }) => {
           </div>
         )}
 
-          <div className="flex justify-between ">
-            <button type="button" onClick={onBack}>
-              Tilbage
-            </button>
+        <div className="flex justify-between ">
+          <button type="button" onClick={onBack}>
+            Tilbage
+          </button>
 
-            <button type="submit">submit</button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
-};
+          <button type="submit">submit</button>
+        </div>
+      </form>
+    </div>
+  </>
+);
 
 export default CampingOptionsForm;
