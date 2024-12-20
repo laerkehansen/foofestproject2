786b7f38b3ecc85a8b40533b8320c09131ba5651
kati0234 <katinkaasp@gmail.com>
2024-12-20 03:44:20 +0100

    comit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "./components/navigation/Header";
-import Head from "next/head";
-import Menu from "./components/navigation/Menu";
+
 import Footer from "./components/navigation/Footer";
 // import ApiContext from "./lib/ApiContext";
 export const metadata = {
@@ -11,12 +10,9 @@ export const metadata = {
 };
 const inter = Inter({
   subsets: ["latin"],
-  // weight: ["400", "500", "600", "700"],
 
   display: "swap",
   variable: "--font-inter",
-  // style: ["normal", "italic"], // Normal og Italic
-  // variable: "--font-inter", // Til Tailwind variable
 });
 
 export default function RootLayout({ children }) {
@@ -24,8 +20,6 @@ export default function RootLayout({ children }) {
     <html lang="en" className={inter.variable}>
       <body className="bg-background font-sans ">
         <Header />
-        {/* <Menu /> */}
-        {/* <Navbar></Navbar> */}
 
         {children}
         <Footer />
